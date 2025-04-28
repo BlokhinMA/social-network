5adc236487c495c72690a54917f27f593edee47e
bspil <b.spillbert2016@gmail.com>
2025-04-28 22:25:22 +0400

    fixed cors policy

--- socialnetworkbackend/src/main/java/ru/sstu/socialnetworkbackend/configs/SecurityConfig.java
+++ socialnetworkbackend/src/main/java/ru/sstu/socialnetworkbackend/configs/SecurityConfig.java
@@ -3,12 +3,18 @@ package ru.sstu.socialnetworkbackend.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(requests -> requests
                                 .requestMatchers("/api/registration/register", "/api/auth").permitAll()
                                 .anyRequest().authenticated()
@@ -37,6 +44,20 @@ public class SecurityConfig {
         ;
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration conf = new CorsConfiguration();
+        conf.setAllowedOrigins(List.of("http://localhost:8082"));
+        conf.setAllowedMethods(List.of("GET", "POST", "DELETE", "PATCH"));
+        conf.setAllowedHeaders(List.of("*"));
+        conf.setExposedHeaders(List.of("*"));
+        conf.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
+        src.registerCorsConfiguration("/**", conf);
+        return src;
     }
 
 }
